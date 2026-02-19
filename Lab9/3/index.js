bb479d364a94cdbc2d2c49e70edabe81a4db0c1e
bbimxn <67070102@kmitl.ac.th>
2026-02-19 21:27:10 +0700

    lap9/3

--- Lab9/3/index.js
+++ Lab9/3/index.js
@@ -26,14 +26,12 @@ app.set('view engine', 'ejs');
 // routing path
 
 app.get('/', function (req, res) {
-    const query = 'SELECT * FROM orders;';
+    const query = 'SELECT ID, customer_name, product_name, status FROM orders;'; 
     db.all(query, (err, rows) => {
         if (err) {
             console.error(err.message);
             return res.status(500).send("Database Error!");
         }
-        
-        console.log(rows); 
         res.render('home', { data: rows }); 
     });
 });
@@ -43,7 +41,9 @@ app.get('/create', function (req, res) {
         ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         customer_name TEXT,
         product_name TEXT,
-        status TEXT DEFAULT 'รอดำเนินการ'
+        status TEXT DEFAULT 'รอดำเนินการ',
+        tel TEXT,
+        address TEXT
     );`;
 
     db.run(sql, (err) => {
@@ -54,6 +54,38 @@ app.get('/create', function (req, res) {
     });
 })
 
+app.get('/formget', function (req, res) {
+    const customer_name = req.query.customer_name;
+    const product_name = req.query.product_name;
+    const address = req.query.address;
+    const tel = req.query.tel;
+
+    let sql = `INSERT INTO orders (customer_name, product_name, address, tel) VALUES (?, ?, ?, ?)`;
+
+    db.run(sql, [customer_name, product_name, address, tel], (err) => {
+        if (err) {
+            console.error('Error inserting data:', err.message);
+            return res.status(500).send("Database Error");
+        }
+        console.log('Data inserted successful');     
+        res.redirect('/');
+    });
+});
+
+app.get('/update-status', function (req, res) {
+    const { id, status } = req.query;
+    const sql = `UPDATE orders SET status = ? WHERE ID = ?`;
+
+    db.run(sql, [status, id], (err) => {
+        if (err) {
+            console.error(err.message);
+            return res.status(500).send("Update Error!");
+        }
+        console.log(`Order ${id} updated to ${status}`);
+        res.redirect('/');
+    });
+});
+
 
 // Starting the server
 app.listen(port, () => {
@@ -63,5 +95,5 @@ app.listen(port, () => {
 
 
 
-//INSERT INTO orders (ID, customer_name, product_name, status) VALUES (1,'กนกวรรณ','รองเท้ากีฬา','รอดำเนินการ');
-//INSERT INTO orders (ID, customer_name, product_name, status) VALUES (2,'วิภาดา','กระเป๋าเดินทาง','รอดำเนินการ');
+//INSERT INTO orders (ID, customer_name, product_name, status,tel,address) VALUES (1,'กนกวรรณ','รองเท้ากีฬา','รอดำเนินการ','0235946556','home');
+//INSERT INTO orders (ID, customer_name, product_name, status,tel,address) VALUES (2,'วิภาดา','กระเป๋าเดินทาง','รอดำเนินการ','0684641352','company');
